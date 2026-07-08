53850fff02c103480f2c583061f735ec263e0526
turbo <turbo@dev.local>
2026-07-08 08:26:59 +0800

    feat(nvim): change git.lua

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,7 +8,24 @@ return {
             "BufReadPre",
             "BufNewFile",
         },
-        opts = {},
+        opts = {
+            ------------------------------------------------------------------
+            -- Inline Blame
+            ------------------------------------------------------------------
+            current_line_blame = false,
+            current_line_blame_opts = {
+                delay = 300,
+                virt_text = true,
+                virt_text_pos = "eol",
+                ignore_whitespace = false,
+            },
+            ------------------------------------------------------------------
+            -- Preview
+            ------------------------------------------------------------------
+            preview_config = {
+                border = "rounded",
+            },
+        },
         config = function(_, opts)
             local gs = require("gitsigns")
             gs.setup(opts)
@@ -16,20 +33,50 @@ return {
             ------------------------------------------------------------------
             -- Navigation
             ------------------------------------------------------------------
-            map("n", "]h", gs.next_hunk, { desc = "Next Hunk" })
-            map("n", "[h", gs.prev_hunk, { desc = "Prev Hunk" })
+            map("n", "]h", gs.next_hunk, {
+                desc = "Next Hunk",
+            })
+            map("n", "[h", gs.prev_hunk, {
+                desc = "Prev Hunk",
+            })
             ------------------------------------------------------------------
             -- Hunk
             ------------------------------------------------------------------
-            map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage Hunk" })
-            map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
-            map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage Buffer" })
-            map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset Buffer" })
-            map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+            map("n", "<leader>hs", gs.stage_hunk, {
+                desc = "Stage Hunk",
+            })
+            map("n", "<leader>hr", gs.reset_hunk, {
+                desc = "Reset Hunk",
+            })
+            map("n", "<leader>hS", gs.stage_buffer, {
+                desc = "Stage Buffer",
+            })
+            map("n", "<leader>hR", gs.reset_buffer, {
+                desc = "Reset Buffer",
+            })
+            map("n", "<leader>hp", gs.preview_hunk, {
+                desc = "Preview Hunk",
+            })
+            ------------------------------------------------------------------
+            -- Blame
+            ------------------------------------------------------------------
             map("n", "<leader>hb", function()
                 gs.blame_line({ full = true })
-            end, { desc = "Blame Line" })
-            map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
+            end, {
+                desc = "Blame Line",
+            })
+            map("n", "<leader>gb", gs.toggle_current_line_blame, {
+                desc = "Toggle Line Blame",
+            })
+            ------------------------------------------------------------------
+            -- Diff
+            ------------------------------------------------------------------
+            map("n", "<leader>hd", gs.diffthis, {
+                desc = "Diff This",
+            })
+            map("n", "<leader>hD", gs.toggle_deleted, {
+                desc = "Toggle Deleted",
+            })
         end,
     },
     ----------------------------------------------------------------------
@@ -83,7 +130,12 @@ return {
             {
                 "<leader>gh",
                 "<cmd>DiffviewFileHistory %<CR>",
-                desc = "File History",
+                desc = "Current File History",
+            },
+            {
+                "<leader>gH",
+                "<cmd>DiffviewFileHistory<CR>",
+                desc = "Repository History",
             },
         },
     },
