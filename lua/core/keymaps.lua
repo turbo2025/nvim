a71529ca94d825a7bad0d1a0ece2f2bd914f298d
turbo <turbo@dev.local>
2026-09-09 23:19:40 +0800

    feat(update): add pane key map

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,6 +19,17 @@ map("n", "<leader>wh", "<C-w>h")
 map("n", "<leader>wj", "<C-w>j")
 map("n", "<leader>wk", "<C-w>k")
 map("n", "<leader>wl", "<C-w>l")
+-- 垂直，水平分屏以及关闭
+map("n", "<leader>w|", "<cmd>vsplit<cr>", { desc = "Split Vertical" })
+map("n", "<leader>w-", "<cmd>split<cr>", { desc = "Split Horizontal" })
+map("n", "<leader>wc", "<cmd>close<cr>", { desc = "Close Window" })
+--------------------------------------------------------
+-- Tabpage
+--------------------------------------------------------
+map("n", "[t", "<cmd>tabprevious<cr>", { desc = "Prev Tab" })
+map("n", "]t", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader>Tn", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader>Tc", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 --------------------------------------------------------
 -- Clear Highlight
 --------------------------------------------------------
